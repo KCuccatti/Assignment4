333d3da4dee4de68088ce33cdac1281747ff99ac
Kyle Cuccatti <kylecuccatti@ymail.com>
2018-10-02 22:20:54 -0500

    Final commit

--- logic.js
+++ logic.js
@@ -1,112 +1,183 @@
-$(document).ready(function(){
-
+$(document).ready(function () {
   const stockList = [];
 
-    let stockImg;
-    let stockCompanyName; 
-    let stockSymbol;
-    let stockLatestPrice;
-    const validationList = ajaxGetAllStockSymbols();
-   
+  let stockImg;
+  var stockNews = [];
+  var stockData = [];
+  var stockLogo = [];
 
-  $('#btnSubmit').on('click', function(){
-   
-    // When 'submit' is clicked, clear out the text box. 
-    $('#stockInfo').empty();
+  // Hold onto the list of stocks the user is interested in following
+  const validationList = ajaxGetAllStockSymbols();
 
+  // When a user clicks on one of the stock butons, the stock information is obtained from memory
+  $('#btnArr').on('click', 'button', function () {
+    displayStockFromMemory(this.id);
+  });
+
+  $('#btnSubmit').on('click', function () {
+
+    // Each time the user requests info on a stock, the previous contents are removed from screen
+    $('#tBody').empty();
 
     event.preventDefault();
     stockSymbol = $('#stockInfo').val();
-    const queryURL = `https://api.iextrading.com/1.0/stock/${stockSymbol}/quote`;
-    const queryLogo = `https://api.iextrading.com/1.0/stock/${stockSymbol}/logo`;
-    const queryAllSymbols = `https://api.iextrading.com/1.0/ref-data/symbols`;
-
-    
 
     // Check to see if symbol exists in validationList array. 
-    if(!contains(validationList.responseJSON, "symbol", stockSymbol)) { 
+    // TODO: CHANGE ALERT TO DIV TAG
+    if (!contains(validationList.responseJSON, "symbol", stockSymbol)) {
       alert("Stock symbol was not found.");
       $('#stockInfo').val("");
       return;
-      
     }
 
+    $.when(ajaxGetStockInfo(), ajaxGetStockLogo(), ajaxGetStockNews()).done(function (a1, a2, a3) {
 
-    $.when(ajaxGetStockInfo(), ajaxGetStockLogo()).done(function(a1, a2){
-      let newStockBtn = $('#stockInfo').val().trim();
-      stockList.push(newStockBtn);
-      $('#btnArr').append(`<input id=${newStockBtn} type='button' value=${newStockBtn} </input>`);
+      if (stockList.includes($('#stockInfo').val())) {
+        // Stock Info was already retreived from previous calls so obtain from memory.
+        displayStockFromMemory($('#stockInfo').val());
+      }
+      else {
+        // Use the stock info that was obtained from ajax calls
+        let newStockBtn = $('#stockInfo').val().trim();
+        $('#tBody').empty();
+        stockList.push(newStockBtn);
+        $('#btnArr').append(`<button id="${newStockBtn}" class="stock-button">${stockSymbol}</button>`);
 
-      $('#tBody').append(`<tr><td> ${stockCompanyName} </td><td> ${stockSymbol} 
-          </td><td> ${stockLatestPrice} </td> <td><img src="${stockImg}" height="50"/></td></tr>`)
-          $('#stockInfo').val('');
-      });
-  });
+        for (let i = 0; i < stockData.length; i++) {
+          if (stockData[i].stockSymbol == stockSymbol) {
+            $('#tBody').append(`<tr><td>${stockData[i].stockCompanyName}</td><td>${stockData[i].stockSymbol} 
+                        </td><td>${stockData[i].stockLatestPrice}</td><td><img src="${stockLogo[i].stockImg}" height="50"/></td></tr>`)
+          }
+        }
 
-  // Gets the stock symbol from the API 
+        // Append to the tbody just the news from the stockNews json for the stock being viewed.
+        for (let i = 0; i < stockNews.length; i++) {
+          if (stockNews[i].stockSymbol == stockSymbol) {
+            $('#tBody').append(`<tr><td colspan="4"><a href="${stockNews[i].url}" target="new">${stockNews[i].headline}</a></td></tr>`);
+          }
+        }
+      }
+      $('#stockInfo').val('');
+
+    });
+
+  })
+
+  // Gets the list of valid stocks from the API if information is not already present in memory
   function ajaxGetAllStockSymbols() {
-    return $.ajax({
-      type: "GET",
-      url: `https://api.iextrading.com/1.0/ref-data/symbols`,
-      datatype: "json",
-      success: getAllStockSymbols,
-    });
+    if (stockList.length == 0) {
+      return $.ajax({
+        type: "GET",
+        url: `https://api.iextrading.com/1.0/ref-data/symbols`,
+        datatype: "json",
+        success: getAllStockSymbols,
+      });
+    }
   }
 
-
-  // Grabs the stock information from the API 
+  // Gets the stock information from the API  if information is not already present in memory
   function ajaxGetStockInfo() {
-    return $.ajax({
-      type: "GET",
-      url: `https://api.iextrading.com/1.0/stock/${stockSymbol}/quote`,
-      datatype: "json",
-      success: getStockInfo,
-    });
+    if (!dataAlreadyInList(stockSymbol)) {
+      return $.ajax({
+        type: "GET",
+        url: `https://api.iextrading.com/1.0/stock/${stockSymbol}/quote`,
+        datatype: "json",
+        success: getStockInfo,
+      });
+    }
   }
 
-  // Grabs the stock logo from the API 
+  // Gets the stock news quotes from the API if information is not already present in memory 
+  function ajaxGetStockNews() {
+    if (!dataAlreadyInList(stockSymbol)) {
+      return $.ajax({
+        type: "GET",
+        url: `https://api.iextrading.com/1.0/stock/${stockSymbol}/batch?types=quote,news&range=1m&last=10`,
+        datatype: "json",
+        success: getStockNews,
+      });
+    }
+  }
+
+  // Gets the stock logo from the API if not already present in memory
   function ajaxGetStockLogo() {
-    return $.ajax({
-      type: "GET",
-      url: `https://api.iextrading.com/1.0/stock/${stockSymbol}/logo`,
-      datatype: "json",
-      success: getStockLogo,
-    });
+    if (!dataAlreadyInList(stockSymbol)) {
+      return $.ajax({
+        type: "GET",
+        url: `https://api.iextrading.com/1.0/stock/${stockSymbol}/logo`,
+        datatype: "json",
+        success: getStockLogo,
+      });
+    }
   }
 
-
+  // Gets stock symbols from API 
   function getAllStockSymbols(response) {
     return response;
   }
-  
-  // Gets the stock information from the assigned variables
-  function getStockInfo(response) {
-    stockCompanyName = response.companyName;
-    stockSymbol = response.symbol;
-    stockLatestPrice = response.latestPrice;
-  }
-  
-  // Gets stock logo from assigned variable
-    function getStockLogo (response) {
-      stockImg = response.url;
-  }
-  
 
-  // CHECK TO SEE IF THE ARRAY CONTAINS A GIVEN VALUE FOR A PARTICULAR KEY
-    function contains(arr, key, val) {
-      for (let i=0; i < arr.length; i++) {
-        if (arr[i].symbol == val.toUpperCase()) {
-          return true;
+  // Only add stock information to the array if it has not already been previously fetched.
+  function getStockInfo(response) {
+    let found = false;
+    for (let i = 0; i < stockData.length; i++) {
+      if (stockData[i].stockSymbol == stockSymbol) {
+        found = true;
+        break;
+      }
+    }
+    if (!found) {
+      stockData.push({ stockSymbol: stockSymbol, stockCompanyName: response.companyName, stockLatestPrice: response.latestPrice });
+    }
+  }
+
+  // Add the first three news headlines for the selected stock
+  function getStockNews(response) {
+      for (let i = 0; i < 3; i++) {
+        let headline = response.news[i].headline;
+        let url = response.news[i].url;
+        stockNews.push({ stockSymbol: stockSymbol, headline: headline, url: url });
+      }
+    return stockNews;
+  }
+
+
+  // Assigns stockImg to the img url in the API 
+  function getStockLogo(response) {
+    stockLogo.push({ stockImg: response.url });
+  }
+
+  // Check to see if the array contains a given value for a particular key
+  function contains(arr, key, val) {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i].symbol == val.toUpperCase()) {
+        return true;
       }
     }
     return false;
   }
 
+  function dataAlreadyInList(id) {
+    return stockList.includes(id);
+  }
 
-  // Clears the table of stock information when 'clear' is clicked
-    $('#clear').on('click', function() {
-      ('#tBody').empty();
-  })
+  // Display all information about a stock from memory
+  function displayStockFromMemory(stockSymbol) {
+
+    $('#tBody').empty();
+
+    for (let i = 0; i < stockData.length; i++) {
+      if (stockData[i].stockSymbol == stockSymbol) {
+        $('#tBody').append(`<tr><td>${stockData[i].stockCompanyName}</td><td>${stockData[i].stockSymbol} 
+                    </td><td>${stockData[i].stockLatestPrice}</td><td><img src="${stockLogo[i].stockImg}" height="50"/></td></tr>`)
+      }
+    }
+
+    // Append to the tbody just the news from the stockNews json for the stock being viewed.
+    for (let i = 0; i < stockNews.length; i++) {
+      if (stockNews[i].stockSymbol == stockSymbol) {
+        $('#tBody').append(`<tr><td colspan="4"><a href="${stockNews[i].url}" target="new">${stockNews[i].headline}</a></td></tr>`);
+      }
+    }
+  }
 
 })
-
